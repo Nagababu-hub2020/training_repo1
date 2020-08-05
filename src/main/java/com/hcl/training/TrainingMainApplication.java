4d110dce385f46fa014676eb21f6f92509ea4610
Nagababu-hub2020 <61000823+Nagababu-hub2020@users.noreply.github.com>
2020-08-05 16:50:00 +0530

    Update TrainingMainApplication.java
    Modified the logic

--- src/main/java/com/hcl/training/TrainingMainApplication.java
+++ src/main/java/com/hcl/training/TrainingMainApplication.java
@@ -2,15 +2,20 @@ package com.hcl.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+
 @EnableSwagger2
 @SpringBootApplication
-public class TrainingMainApplication {
+public class TrainingMainApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrainingMainApplication.class, args);
@@ -19,6 +24,18 @@ public class TrainingMainApplication {
 	@Bean
 	public Docket applicationApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.hcl.training")).build();
+				.apis(RequestHandlerSelectors.basePackage("com.hcl.training")).build()
+				.apiInfo(metaInfo());
+	}
+	private ApiInfo metaInfo() {
+		ApiInfo apiInfo = new ApiInfo("Swagger-REST Application ",
+				" swagger with rest services ",
+				"1.0 ",
+				"terms of service ",
+				new Contact("", "", ""),
+				"",
+				"",
+				new ArrayList());
+		return apiInfo;
 	}
 }
